a8b5ced1c230be257fd53f53d397763c92a88305
Masala <masala@riseup.net>
2018-10-19 09:39:56 -0400

    Add voting Authorities to playground

--- settings.go
+++ settings.go
@@ -16,6 +16,12 @@
 
 package playground
 
+import (
+	"github.com/katzenpost/core/crypto/ecdh"
+	"github.com/katzenpost/core/crypto/eddsa"
+	"github.com/katzenpost/authority/voting/server/config"
+)
+
 const (
 	// NOTICE: change me to correct playground information
 	RegistrationAddr      = "idefix.katzenpost.mixnetworks.org:39484"
@@ -26,3 +32,37 @@ const (
 	OnionAuthorityAddr    = "l4lqji3jlkcul23w.onion:29485"
 	AuthorityPublicKey    = "39Xhom6bPvez2gECACuTxm/DaxLRTGCMP7/KA78+vNw="
 )
+
+func b64eddsa(a string) *eddsa.PublicKey {
+	k := new(eddsa.PublicKey)
+	if err := k.FromString(a); err != nil {
+		return nil
+	}
+	return k
+}
+
+func b64ecdh(a string) *ecdh.PublicKey {
+	k := new(ecdh.PublicKey)
+	if err := k.FromString(a); err != nil {
+		return nil
+	}
+	return k
+}
+
+var Authorities []*config.AuthorityPeer = []*config.AuthorityPeer{
+	&config.AuthorityPeer{
+		Addresses: []string{"103.104.244.148:30000",},
+		IdentityPublicKey: b64eddsa("EmUWxb6ocBBXhxlrAKgxVd/6tyIDVK/8pIY/nZrqSDQ="),
+		LinkPublicKey: b64ecdh("LGw+ZqpN6KmGErMmliRHRUFwWwhr6d8WZWNMkjHerAQ="),
+	},
+	&config.AuthorityPeer{
+		Addresses: []string{"103.104.244.146:30000",},
+		IdentityPublicKey: b64eddsa("vdOAeoRtWKFDw+W4k3sNN1EMT9ZsaHHmuCHOEKSg1aA="),
+		LinkPublicKey: b64ecdh("zLzr8HIFHrZzRxIMsjhxhRDCu+UVnlxo8KbtLsOkrH8="),
+	},
+	&config.AuthorityPeer{
+		Addresses: []string{"103.104.244.147:30000",},
+		IdentityPublicKey: b64eddsa("bFgvws69dJrc3ACKXN5aCJKLHjkN7D8DA2HDKkhSNIk="),
+		LinkPublicKey: b64ecdh("CZQadnvwlhpzJI+foNBru3odteb+tCn1n+sSfJtRc34="),
+	},
+}
